04bd021767b2b44c4321a5931ee87d86bf187cc3
OlegKechin <oleg_kecin@mail.ru>
2024-05-11 19:18:51 +0500

    add test

---
+++ src/test/java/HorseTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+
+class HorseTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getSpeed() {
+    }
+
+    @Test
+    void getDistance() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void getRandomDouble() {
+    }
+}
